278e5daa8c97e6d7714e6029c10d45f83e90fa94
Tiago de Santos <tiagodmsanto@gmail.com>
2026-02-05 21:20:46 -0300

    Exemplo 2 editado

--- int.log.comp/tema2/exemplo2.c
+++ int.log.comp/tema2/exemplo2.c
@@ -1,5 +1,32 @@
-#inlcude <stdio.h>
+#include <stdio.h>
+
+/* Inicio de programa*/
 
 int main(){
- printf("Boas vindas a calculadora de médias"); 
+ printf("Boas vindas a calculadora de médias\n"); 
+
+/*Declaração de dados*/
+
+float nota1, nota2, nota3, media;
+
+/*Entrada de dados*/
+
+printf("Digite a primeira nota: ");
+scanf("%f", &nota1);
+
+printf("Digite a segunda nota: ");
+scanf("%f",&nota2);
+
+printf("Digite a terceira nota: ");
+scanf("%f",&nota3);
+
+/*Processamento de dados*/
+
+media = (nota1+nota2+nota3)/3;
+
+/*Saída de dados*/
+
+printf("A média é: %.2f", media);
+
+return 0;
 }
