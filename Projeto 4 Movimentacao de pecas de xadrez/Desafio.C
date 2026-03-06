4bb44ede331aa77651481bf4948d3f0b82463f27
Tiago de Melo Santos <202601281331@alunos.unifanor.edu.br>
2026-03-06 16:34:48 -0300

    Corretivas e implementações

--- Projeto 4 Movimentacao de pecas de xadrez/Desafio.C
+++ Projeto 4 Movimentacao de pecas de xadrez/Desafio.C
@@ -1,31 +1,56 @@
 #include <stdio.h>
 
+#define casas_bisto 5
+#define casas_rainha 8
+#define casas_torre 5
+#define casas_cavalo 3
+
+      //Funções Recursivas
+
+void mover_torre(int t, int atual) {
+    if (atual <= t) {
+        printf ("A torre foi movida para a posição %d direita\n", atual);
+        mover_torre(t, atual+1);
+    }
+}
+
+void mover_rainha(int r, int atual) {
+    if (atual <= r) {
+        printf ("A rainha foi movida para a posição %d esquerda\n", atual);
+        mover_rainha(r, atual+1);
+    }
+}
+
+
+
 int main() {
 
-    int direcaotorre = 1; // Variável para controlar a direção da torre
-    int direcaobispo = 1; // Variável para controlar a direção do bispo
-    int direcaorainha = 1; // Variável para controlar a direção da rainha
-    // Movimentação das peças
+    //Chamada das funções recursivas
 
-    do {
-        printf("A torre está na posição horizontal %d.\n", direcaotorre);
-        direcaotorre ++;
-        printf("A torre está na posição horizontal %d a direita.\n", direcaotorre);
-    } while ( direcaotorre <= 4);
+    printf("Movimentação da Torre:\n");
+    mover_torre(casas_torre, 1);
+    printf("\nMovimentação da Rainha:\n");
+    mover_rainha(casas_rainha, 1);
 
-    do {
-        printf ("A rainha está na posição %d a esquerda.\n", direcaorainha);
-        direcaorainha ++;       
-        printf ("A rainha está na posição %d a esquerda.\n", direcaorainha);
-    }   while (direcaorainha <= 7);
+    //Funções Iterativas
+    
+    printf("\nMovimentação do bispo\n");
+    for (int i=1; i<=casas_bisto; i++){
+        for (int j=1; j<=1; j++) {
+            printf("O bispo está na posição %d direita.\n", i);
+        }
+        printf("O bispo está na posição %d cima.\n", i);
+    }
 
-    do {
-        printf ("O bispo está na posição %d.\n", direcaobispo);
-        printf("Casa %d: Cima, Direita\n", direcaobispo);
-        
-        direcaobispo++; // Incrementa para a próxima casa
-        
-    } while (direcaobispo <= 5);
-
-    return 0;
+    printf("\nMoivmentação do cavalo\n");
+    for (int i=1; i<=3;i++) {
+        int j= 0;
+           while (j<2) {
+            printf("O cavalo está na posição %d cima.\n", j);
+            j++;  
+           }
+            printf("O cavalo está na posição %d direita.\n", i);
+    }
+          
+return 0;
 }
